5ed8790aa76756e842a0d232dc96ddf84bea5e3d
AnnMariyJoy <150906766+AnnMariyJoy@users.noreply.github.com>
2024-03-01 22:23:47 +1200

    program No9

---
+++ S2/ANC/pro9.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+factorial(){
+	num=$1
+	fact=1
+	for((i=1;i<=num;i++));do
+		fact=$((fact * i))
+	done
+	echo "Factorial of $num is: $fact"
+}
+read -p"Enter a number:" input_number
+factorial $input_number

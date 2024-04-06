61ed6249d3795779ebb5f46a5d4725f07b05578c
AnnMariyJoy <150906766+AnnMariyJoy@users.noreply.github.com>
2024-04-06 14:29:42 +0530

    Delete S2/JAVA/mul.java

--- S2/JAVA/mul.java
+++
@@ -1,8 +0,0 @@
-public class mul extends Thread{
-    public void run(){
-        System.out.println("\n");
-        for(int i =0;i<11;i++){
-            System.out.println("5*"+i+" = "+5*i);
-        }
-    }
-}
